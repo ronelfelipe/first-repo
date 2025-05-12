8d15cbf389028f0a5490111895ca6bec6d4576d0
ronelfelipe <ronel.felipe.2811@outlook.com>
2025-05-11 20:27:41 -0500

    Initial commit

---
+++ 00_hello.py
@@ -0,0 +1,20 @@
+# Hola mundo
+print("Hola Python")
+print('Hola Python')
+"""
+Este es
+un comentario
+de varias lineas
+"""
+
+'''
+Este es 
+un comentario
+en varias lineas
+'''
+
+# Consultar el tipo de dato
+print(type("Hola mundo"))  # tipo str
+print(type(5)) # tipo int
+print(type(4.5)) # tipo float
+print(type(True))
---
+++ 01_variables.py
@@ -0,0 +1,10 @@
+# Variables
+
+MyVariable = "My String variable"
+
+print(MyVariable)
+
+# Funciona pero por convension debe ser todo minuscula y con snakecase (unidos con _)
+
+my_variable = "My String variable"
+print(my_variable)
